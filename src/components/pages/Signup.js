93c7188bd94733e188771e987ecc926f052d77ea
Rohan Darade <rohandarade29@gmail.com>
2023-10-23 01:35:01 +0530

    signup

---
+++ src/components/pages/Signup.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function Signup() {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/users",
+        data
+      );
+      console.log(response);
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          placeholder="Name"
+          value={data.name}
+          onChange={(e) => setData({ ...data, name: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          value={data.email}
+          onChange={(e) => setData({ ...data, email: e.target.value })}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={data.password}
+          onChange={(e) => setData({ ...data, password: e.target.value })}
+        />
+        <button onClick={handleSubmit}>Signup</button>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
